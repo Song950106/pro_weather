e25b7f8a608c9543f7455ddc20a0ce6775f15340
song <song950106@outlook.com>
2020-07-06 23:16:09 +0800

    test flutter methodChannel in android pro

--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.weather;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -8,21 +9,43 @@ import android.view.View;
 import android.widget.FrameLayout;
 
 import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugins.GeneratedPluginRegistrant;
 import io.flutter.view.FlutterView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FlutterActivity {
 
     private final String TAG = "MainActivity";
+    private final String CHANNEL = "com.example.weather.weatherData";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate: ===================");
-        startActivity(
-                FlutterActivity
-                        .withNewEngine()
-                        .initialRoute("/")
-                        .build(MainActivity.this)
+//        startActivity(
+//                FlutterActivity
+//                        .withNewEngine()
+//                        .initialRoute("/")
+//                        .build(MainActivity.this)
+//        );
+        GeneratedPluginRegistrant.registerWith(MyApplication.getFlutterEngine());
+    }
+
+    @Override
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+        super.configureFlutterEngine(flutterEngine);
+        MethodChannel methodChannel = new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(),CHANNEL);
+        methodChannel.setMethodCallHandler(
+                (call, result) -> {
+                    if ("getWeatherData".equals(call.method)) {
+                        //TODO
+                        result.success("=======  test channel ========");
+                    } else {
+                        result.notImplemented();
+                    }
+                }
         );
     }
 }
--- app/src/main/java/com/example/weather/MyApplication.java
+++ app/src/main/java/com/example/weather/MyApplication.java
@@ -7,12 +7,16 @@ import io.flutter.embedding.engine.FlutterEngineCache;
 import io.flutter.embedding.engine.dart.DartExecutor;
 
 public class MyApplication extends Application {
+    private static FlutterEngine flutterEngine;
+    public static FlutterEngine getFlutterEngine(){
+        return flutterEngine;
+    }
 
     @Override
     public void onCreate() {
         super.onCreate();
         // Instantiate a FlutterEngine.
-        FlutterEngine flutterEngine = new FlutterEngine(this);
+        flutterEngine = new FlutterEngine(this);
         // Configure an initial route.
         flutterEngine.getNavigationChannel().setInitialRoute("/");
         // Start executing Dart code to pre-warm the FlutterEngine.
