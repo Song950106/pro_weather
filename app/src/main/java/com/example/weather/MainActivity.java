46aa0e5d68c53a40d99c2d42d45460a170289235
song <song950106@outlook.com>
2020-10-09 16:50:15 +0800

    update submodule && modify weather data api

--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -1,6 +1,4 @@
 package com.example.weather;
-
-
 import androidx.annotation.NonNull;
 
 import android.os.Bundle;
@@ -28,12 +26,17 @@ public class MainActivity extends BoostFlutterActivity {
     public final String EVENT_CHANNEL = "com.example.weather.eventChannel";
     public final String QUERY_TYPE_WEEKLY = "7d";
     public final String QUERY_TYPE_DAILY = "24h";
+    public final String QUERY_TYPE_NOW = "now";
     public final String KEY = "2869245148564fd0a43251b16a168cbd";
     public String responseBody ;
     public MethodChannel methodChannel;
     public EventChannel eventChannel;
     public EventChannel.EventSink eventSink;
 
+
+    final String GET_WEATHER_DAILY = "getWeatherDaily";
+    final String GET_WEATHER_WEEKLY = "getWeatherWeekly";
+    final String GET_WEATHER_NOW = "getWeatherNow";
 
 
     @Override
@@ -56,10 +59,18 @@ public class MainActivity extends BoostFlutterActivity {
         methodChannel = new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(),METHOD_CHANNEL);
         methodChannel.setMethodCallHandler(
                 (call, result) -> {
-                    if ("getWeatherData".equals(call.method)) {
-                        testNetwork(result);
-                    } else {
-                        result.notImplemented();
+                    switch (call.method){
+                        case GET_WEATHER_DAILY:
+                            getWeatherData(result,1);
+                            break;
+                        case GET_WEATHER_NOW:
+                            getWeatherData(result,0);
+                            break;
+                        case GET_WEATHER_WEEKLY:
+                            getWeatherData(result,2);
+                            break;
+                        default:
+                            result.notImplemented();
                     }
                 }
         );
@@ -80,21 +91,67 @@ public class MainActivity extends BoostFlutterActivity {
         );
     }
 
+    private void getWeatherData(MethodChannel.Result result,int flag ){
+        String dataType = "";
+        switch (flag){
+            case 0:
+                dataType = QUERY_TYPE_NOW;
+                break;
+            case 1:
+                dataType = QUERY_TYPE_DAILY;
+                break;
+            case 2:
+                dataType = QUERY_TYPE_WEEKLY;
+                break;
+        }
+        Log.d(TAG, "getWeatherDaily: dataType " + dataType);
+        RetrofitUtils.getApiService(WeatherApi.class,"https://devapi.heweather.net/").
+                getWeatherData(dataType,"101010100",KEY).
+                subscribeOn(Schedulers.io()).
+                observeOn(AndroidSchedulers.mainThread()).
+                subscribe(new Observer<String>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(String value) {
+                        Log.d(TAG, "onNext: " + value);
+                        // no error
+                        result.success(0);
+                        eventSink.success( flag + value);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        result.success(-1);
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+
+    }
+
     public void  testNetwork(MethodChannel.Result result){
-        Log.d(TAG, "testNetwork: ==============");
+
 
 //        Observable<String> city_call = RetrofitUtils.getApiService(WeatherApi.class,"https://geoapi.heweather.net/").
 //                getCity("beijing",KEY,"fuzzy");
 
         // network is unavailable
-        if(!MyApplication.isOnline()){
-            Log.d(TAG, "testNetwork: status ");
-            result.success(-1);
-            return;
-        }
+//        if(!MyApplication.isOnline()){
+//            Log.d(TAG, "testNetwork: status ");
+//            result.success(-1);
+//            return;
+//        }
 
+        // 24h weather data
         RetrofitUtils.getApiService(WeatherApi.class,"https://devapi.heweather.net/").
-                getWeatherData(QUERY_TYPE_WEEKLY,"101010100",KEY).
+                getWeatherData(QUERY_TYPE_DAILY,"101010100",KEY).
                 subscribeOn(Schedulers.io()).
                 observeOn(AndroidSchedulers.mainThread()).
                 subscribe(new Observer<String>() {
